c8b0f9f32a9eb76898687bcc8cdaef17fb02b33c
gotpaid1313 <161022802+gotpaid1313@users.noreply.github.com>
2024-02-23 14:58:28 +0300

    Update InstructionsComponent.jsx

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -16,11 +16,11 @@ export default function InstructionsComponent() {
 			<div className={styles.buttons_container}>
 				<a
 					target={"_blank"}
-					href={"https://createweb3dapp.alchemy.com/#components"}
+					href={"chrome-extension://egjidjbpglichdcondbcbdnbeeppgdph/home.html#/onboarding"}
 				>
 					<div className={styles.button}>
 						{/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
-						<p>Add Components</p>
+						<p>Trust wallet</p>
 					</div>
 				</a>
 				<a
